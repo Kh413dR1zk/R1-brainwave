5420cda3e95e3aeb77e091672fdf653a4a73978f
KhaledRizk <kh413d413@gmail.com>
2024-09-23 22:26:36 +0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import RoadMap from "./components/RoadMap";
 import Services from "./components/Services";
 
 const App = () => {
@@ -15,7 +17,9 @@ const App = () => {
         <Benefits />
         <Collaboration />
         <Services />
-        <Pricing/>
+        <Pricing />
+        <RoadMap />
+        <Footer />
       </div>
 
       <ButtonGradient />
